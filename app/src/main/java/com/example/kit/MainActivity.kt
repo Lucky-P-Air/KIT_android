b4b51ba6fde06686bbe4211f32e4bcb929125233
Matt Pereira <Mattcpereira@gmail.com>
2023-02-03 00:28:21 -0700

    Instantiate a navController. Revise how the navController is retrieved: -Remove val navController = findNavController(R.id.nav_host_fragment_activity_main) -Replace with creating navHostFragment & getting its .navController property. Override onSupportNavigateUp() function to execute with navController, or else revert to the super() default.

--- app/src/main/java/com/example/kit/MainActivity.kt
+++ app/src/main/java/com/example/kit/MainActivity.kt
@@ -1,32 +1,52 @@
 package com.example.kit
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.kit.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Define View Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Reference to Bottom Nav View in MainActivity's layout
         val navView: BottomNavigationView = binding.navView
+        Log.d("MainActivity", "ActivityCreated. Bindings complete")
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // Create reference to NavHostFragment, and get its NavController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navController = navHostFragment.navController
+        Log.d("MainActivity", "navController defined successfully")
+
+        /* AppBar configuration setup
+           Passing each menu ID as a set of Ids because each
+           menu should be considered as top level destinations. */
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+                R.id.navigation_home, R.id.navigation_contactlist, R.id.navigation_addContact))
         setupActionBarWithNavController(navController, appBarConfiguration)
+        Log.d("MainActivity", "ActionBar Setup successfully")
+
+        // Setup of bottom navigation NavView
         navView.setupWithNavController(navController)
+        Log.d("MainActivity", "navView setup successfully")
+    }
+
+    // Enable 'Navigate Up', or the in-app Back button (Not the Android button)
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
