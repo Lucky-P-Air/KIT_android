43df17e50d12ee1f2350e0420d451dc26ba13ec2
Matt Pereira <Mattcpereira@gmail.com>
2023-02-05 11:53:46 -0700

    Revised: MainActivity.kt          HomeFragment.kt
    Replace the way the AppBar menu is configured into the MainActivity and different views.
    enableAppBarMenu() function added to MainActivity.kt to have the option appear throughout the app usage.
    This replaces the deprecated setHasOptionsMenu() & onOptionsMenuItemSelected() methodology.

--- app/src/main/java/com/example/kit/MainActivity.kt
+++ app/src/main/java/com/example/kit/MainActivity.kt
@@ -2,15 +2,21 @@ package com.example.kit
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewTreeLifecycleOwner
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.kit.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -22,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         // Define View Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        enableAppBarMenu()
 
         // Reference to Bottom Nav View in MainActivity's layout
         val navView: BottomNavigationView = binding.navView
@@ -30,23 +37,41 @@ class MainActivity : AppCompatActivity() {
         // Create reference to NavHostFragment, and get its NavController
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         navController = navHostFragment.navController
-        Log.d("MainActivity", "navController defined successfully")
-
-        /* AppBar configuration setup
-           Passing each menu ID as a set of Ids because each
-           menu should be considered as top level destinations. */
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.accountFragment))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        Log.d("MainActivity", "ActionBar Setup successfully")
+        Log.d(TAG, "navController defined successfully")
 
         // Setup of bottom navigation NavView
         navView.setupWithNavController(navController)
-        Log.d("MainActivity", "navView setup successfully")
+        Log.d(TAG, "navView setup successfully")
     }
 
     // Enable 'Navigate Up', or the in-app Back button (Not the Android button)
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun enableAppBarMenu() {
+        // CODE SNIPPET FOR REPLACING DEPRECATED setHasOptionsMenu() used within Fragments
+
+        // The usage of an interface lets you inject your own implementation
+        val menuHost: MenuHost = this //requireActivity() if used in Fragments
+        // Add menu items without using the Fragment Menu APIs
+        // Note how we can tie the MenuProvider to the viewLifecycleOwner
+        // and an optional Lifecycle.State (here, RESUMED) to indicate when
+        // the menu should be visible
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.app_bar_menu, menu)
+                Log.d(TAG,"onCreateMenu called")
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                //TODO: Implement navigation to user account settings page
+                Log.d(TAG,"onMenuItemSelected called")
+                return true
+            }
+        }, ViewTreeLifecycleOwner.get(binding.root)!!, Lifecycle.State.STARTED)
+        // replace binding.root with another view if used within a Fragment or subview
     }
 }
--- app/src/main/java/com/example/kit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/kit/ui/home/HomeFragment.kt
@@ -1,13 +1,15 @@
 package com.example.kit.ui.home
 
 import android.os.Bundle
-import android.view.*
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.kit.R
 import com.example.kit.databinding.FragmentHomeBinding
-
+private const val TAG = "HomeFragment"
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
@@ -15,46 +17,6 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // adding this overrided function just to experiment with loading the appBar
-        setHasOptionsMenu(true)
-        /* CODE SNIPPET FOR REPLACING DEPRECATED setHasOptionsMenu()
-        // The usage of an interface lets you inject your own implementation
-        val menuHost: MenuHost = requireActivity()
-
-        // Add menu items without using the Fragment Menu APIs
-        // Note how we can tie the MenuProvider to the viewLifecycleOwner
-        // and an optional Lifecycle.State (here, RESUMED) to indicate when
-        // the menu should be visible
-        menuHost.addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                // Add menu items here
-                menuInflater.inflate(R.menu.layout_menu, menu)
-            }
-
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                // Handle the menu selection
-                return true
-            }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
-        */
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        // return super.onCreateOptionsMenu(menu)
-        inflater.inflate(R.menu.app_bar_menu, menu)
-    }
-
-    /*
-    @Deprecated("Deprecated in Java")
-    override fun onOptionsItemSelected(item: MenuItem): { // Boolean {
-        // return super.onOptionsItemSelected(item)
-        //TODO: Implement navigation to user account settings
-
-    }
-    */
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -67,15 +29,46 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // Observer to retrieve text from HomeViewModel
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // enableAppBarMenu()  // Used to apply AppBar Menu to this fragment only
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        Log.d(TAG, "Home view destroyed")
     }
+
+    /* Currently being used at the MainActivity Level instead of Fragments
+    private fun enableAppBarMenu() {
+        // CODE SNIPPET FOR REPLACING DEPRECATED setHasOptionsMenu()
+
+        // The usage of an interface lets you inject your own implementation
+        val menuHost: MenuHost = requireActivity()
+        // Add menu items without using the Fragment Menu APIs
+        // Note how we can tie the MenuProvider to the viewLifecycleOwner
+        // and an optional Lifecycle.State (here, RESUMED) to indicate when
+        // the menu should be visible
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.app_bar_menu, menu)
+                Log.d(TAG,"onCreateMenu called")
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                Log.d(TAG,"onMenuItemSelected called")
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+    */
 }
