9cd353230a2339f374c351a40390b9a2b2352259
Matt Pereira <Mattcpereira@gmail.com>
2023-02-06 11:43:05 -0700

    Implement LiveData for generating the full ContactListViewModel.list based on the ContactSource().loadContacts()
    Adding getContactList() function to refresh the list as needed.

--- app/src/main/java/com/example/kit/ui/contactlist/ContactListViewModel.kt
+++ app/src/main/java/com/example/kit/ui/contactlist/ContactListViewModel.kt
@@ -3,6 +3,8 @@ package com.example.kit.ui.contactlist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.kit.data.ContactSource
+import com.example.kit.model.Contact
 
 class ContactListViewModel : ViewModel() {
 
@@ -11,5 +13,20 @@ class ContactListViewModel : ViewModel() {
     }
     val text: LiveData<String> = _text
 
-// TODO: Implement the ViewModel
+    // Retrieve list of Contacts data
+    private lateinit var _list: MutableLiveData<MutableList<Contact>>
+    val list: LiveData<MutableList<Contact>> get() = _list
+
+    init {
+        getContactList()
+    }
+
+    // Load or refresh full list of contacts
+    fun getContactList() {
+        _list = MutableLiveData(ContactSource().loadContacts())
+    }
+
+    // fun addContact() {}
+
+    // fun getContactDetail() {}
 }
