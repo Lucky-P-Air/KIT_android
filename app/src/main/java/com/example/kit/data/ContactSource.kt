a2aab0d669501b521053a2625e632ba8f4c00e4a
Matt Pereira <Mattcpereira@gmail.com>
2023-02-20 09:23:01 -0700

    Anonymize mock data

--- app/src/main/java/com/example/kit/data/ContactSource.kt
+++ app/src/main/java/com/example/kit/data/ContactSource.kt
@@ -2,29 +2,58 @@ package com.example.kit.data
 
 import com.example.kit.model.Contact
 
-class ContactSource {
+class ContactSource() {
     /* Builds a list of Contact objects filled with mock data using
     .loadContacts()
      */
+    //var sourceContactList: MutableList<Contact> = loadContacts()
+
+    // Build a list of API-received contacts
     var sourceContactList: MutableList<Contact> = loadContacts()
 
     private fun loadContacts(): MutableList<Contact> {
         //TODO: Alphabetize the list that's returned
         return mutableListOf(
-            Contact("Matt", "Pereira", "mattcpereira@gmail.com", "+13018732741", 2, "days"),
-            Contact("BamBam", "Hermetet", "mattcpereira@gmail.com", "+13018732741", 2, "weeks"),
-            Contact("Lucky", "Pierre", "streetbllplaya@gmail.com", "+12054339837", 4, "days"),
-            Contact("Jean", "Weatherax", "fakeJean@gmail.com", "+852028732741", 1, "days"),
-            Contact("Isaac", "Askew", "fakeIsaac@gmail.com", "+13038732741", 1, "months"),
-            Contact("Celina", "Pereira", "mattcpereira@gmail.com", "+13018732741", 2, "weeks"),
-            Contact("Cira", "Pereira", "streetbllplaya@gmail.com", "+12054339837", 4, "days"),
-            Contact("Sarah", "Taylor", "fakeJean@gmail.com", "+852028732741", 1, "days"),
-            Contact("Bizhan", "Zhumagali", "fakeIsaac@gmail.com", "+13038732741", 1, "months"),
-            Contact("Justin", "Pearse", "mattcpereira@gmail.com", "+13018732741", 2, "weeks"),
-            Contact("Mike", "Neuberger", "streetbllplaya@gmail.com", "+12054339837", 4, "days"),
-            Contact("Sean", "Devlin", "fakeJean@gmail.com", "+852028732741", 1, "days"),
-            Contact("Jayme", "Kogel", "fakeIsaac@gmail.com", "+13038732741", 1, "months")
+            Contact("Gibran", "Alcocer", "Idea10@gmail.com", "+18675309", 2, "days"),
+            Contact("BamBam", "Ruder", "bambam@gmail.com", "+18675309", 2, "weeks"),
+            Contact("Lucky", "Pierre", "threeDeer@gmail.com", "+12058675309", 4, "days"),
+            Contact("Brandon", "Sanderson", "truthless@gmail.com", "+852028675309", 1, "days"),
+            Contact("Will", "Wight", "iteration110@gmail.com", "+13308675309", 1, "months"),
+            Contact("Cixin", "Liu", "threebody@gmail.com", "+12058675309", 4, "days"),
+            Contact("Bill", "Watterson", "calvin@hobbes.com", "+852208675309", 1, "days"),
+            Contact("Cher", "", "sonnysEx@gmail.com", "+13308675309", 1, "months"),
+            Contact("Philter", "", "blossom@chronicles.com", "+13108675309", 2, "weeks"),
+            Contact("Meursault", "", "albert@camus.com", "+12508675309", 4, "days"),
+            Contact("Eithan", "Aurelius", "iSee@ozriel.com", "+12508675309", 4, "days"),
         )
-
     }
+/* Moved to ViewModel for continuous updating
+    fun loadApiContacts(): MutableList<Contact> {
+        var listContacts = loadContacts()
+        scope.launch {
+            try {
+                val response = ContactApi.retrofitService.getContacts(Secrets().headers)
+                val listContactEntries = response.data
+                Log.d("ContactSource", "Get request successful, retrieved ${listContactEntries.size} entries")
+                // Transform nested ContactEntry structure to flat Contact class
+                listContacts = listContactEntries.map() {
+                    Contact(
+                        it.attributes.firstName,
+                        it.attributes.lastName,
+                        it.attributes.email,
+                        it.attributes.phoneNumber,
+                        it.attributes.intervalNumber,
+                        it.attributes.intervalUnit
+                    )
+                }.toMutableList()
+                Log.d("ContactSource", "Transform produced listContacts of size ${listContacts.size} entries")
+            //return (response.data).sortedWith(byFirstName).toMutableList()
+            } catch (e: java.lang.Exception) {
+                Log.d("ContactSource", e.toString())
+            }
+        }
+        return listContacts
+    }
+*/
 }
+
