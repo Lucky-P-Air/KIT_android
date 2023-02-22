104831a073064b1b4e414400dd929506bb87b128
Matt Pereira <Mattcpereira@gmail.com>
2023-02-22 08:47:33 -0700

    Clean up ContactListAdapter. Move functions to expand DateUtils.kt
    TODO: Still considering moving the type-casting of DateTimeStrings received from HTTP Response into the step where Contacts are constructed. (So changing Contact property types for the various dates from Strings to Date, LocalDate, or Instance)
    
    ContactListAdapter.kt
    -Clean up onBindViewHolder by abstracting away some date processing logic & functions. Moved them into DateUtils.kt (getNextContactLocalDate, parseDates, formatDates, parseLocalDates, formatLocalDates)
    -Convert the databinding to process LocalDates (instead of Dates) into strings. Preparation for probably refactoring the Contact property data types from data Strings to LocalDates upon instantiation of Contacts.
    
    DateUtils.kt expanded and left as module instead of a class
    -getNextContactLocalDate(contact) was moved/created here for finding the next reminder date based on lastContacted/createdAt date
    -parseDates(), parseLocalDates(), formatDates(), formatLocalDates() moved here

--- app/src/main/java/com/example/kit/ui/contactlist/ContactListAdapter.kt
+++ app/src/main/java/com/example/kit/ui/contactlist/ContactListAdapter.kt
@@ -10,8 +10,9 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kit.databinding.ListContactBinding
 import com.example.kit.model.Contact
-import com.example.kit.utils.DateUtils
-import java.text.SimpleDateFormat
+import com.example.kit.utils.formatLocalDates
+import com.example.kit.utils.getNextContactLocalDate
+import com.example.kit.utils.parseLocalDates
 import java.util.*
 
 /**
@@ -61,45 +62,13 @@ class ContactListAdapter(val clickListener: ContactListListener) :
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
         val contact = getItem(position)
+        val nextContactLocalDate = getNextContactLocalDate(contact)
         holder.bind(clickListener, contact)
-        //TODO Delete
-        //val dateStringParser = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'")
-        //val dateFormatter = DateTimeFormatter.ofPattern("MMM d, y")
-
-        // LocalDates of createdAt and lastContact
-        val lastContactLocalDate = contact.lastContacted?.let {DateUtils().asLocalDate(parseDates(it)!!)}
-        val createdLocalDate = DateUtils().asLocalDate(parseDates(contact.createdAt)!!)
-        // Set starting point for nextContactDate based on lastContact or createdAt
-        var nextContactLocalDate = lastContactLocalDate ?: createdLocalDate
-        // Increment nextContactLocalDate
-        when (contact.intervalUnit) {
-            "days" -> nextContactLocalDate = nextContactLocalDate.plusDays(contact.intervalTime.toLong())
-            "weeks" -> nextContactLocalDate = nextContactLocalDate.plusWeeks(contact.intervalTime.toLong())
-            "months" -> nextContactLocalDate = nextContactLocalDate.plusMonths(contact.intervalTime.toLong())
-            "years" -> nextContactLocalDate = nextContactLocalDate.plusYears(contact.intervalTime.toLong())
-        }
-
         holder.binding.apply {
-            cardLastContact.text = contact.lastContacted?.let { formatDates(parseDates(it)!!) } ?: "Never"
-            cardNextContact.text = formatDates(DateUtils().asDate(nextContactLocalDate))
+            cardLastContact.text = contact.lastContacted?.let {
+                formatLocalDates(parseLocalDates(it)) } ?: "Never"
+            cardNextContact.text = formatLocalDates(nextContactLocalDate)
         }
-    }
-
-    private fun formatDates(date_value: Date): String {
-        /** Return a String, formatted "MMM d, y", from an input Date object
-         * */
-        val formatter = SimpleDateFormat("MMM d, y", Locale.getDefault())
-        formatter.timeZone = TimeZone.getDefault()
-        //val formatter = SimpleDateFormat("MMM d, y", )
-        return formatter.format(date_value)
-    }
-    private fun parseDates(date_string: String): Date? {
-        /** Return a Date? object from an input Datetime String that is based in UTC
-         * */
-        val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'", Locale.getDefault())
-        formatter.timeZone = TimeZone.getTimeZone("UTC")
-        //val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'")
-        return formatter.parse(date_string)
     }
 }
 
--- app/src/main/java/com/example/kit/utils/DateUtils.kt
+++ app/src/main/java/com/example/kit/utils/DateUtils.kt
@@ -2,32 +2,95 @@ package com.example.kit.utils
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.example.kit.model.Contact
+import java.text.SimpleDateFormat
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.*
 
-class DateUtils {
+//class DateUtils {
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun asDate(localDate: LocalDate): Date {
-        return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant())
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun asDate(localDateTime: LocalDateTime): Date {
-        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant())
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun asLocalDate(date: Date): LocalDate {
-        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun asLocalDateTime(date: Date): LocalDateTime {
-        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault())
-            .toLocalDateTime()
-    }
+@RequiresApi(Build.VERSION_CODES.O)
+fun asDate(localDate: LocalDate): Date {
+    return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant())
 }
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun asDate(localDateTime: LocalDateTime): Date {
+    return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant())
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun asLocalDate(date: Date): LocalDate {
+    return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate()
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun asLocalDateTime(date: Date): LocalDateTime {
+    return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault())
+        .toLocalDateTime()
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun formatLocalDates(dateValue: LocalDate): String {
+    /** Return a String, formatted "MMM d, y", from an input LocalDate object
+     * */
+    val formatter = DateTimeFormatter.ofPattern("MMM d, y").withZone(ZoneId.systemDefault())
+    return dateValue.format(formatter)
+}
+
+fun formatDates(dateValue: Date): String {
+    /** Return a String, formatted "MMM d, y", from an input Date object
+     * */
+    val formatter = SimpleDateFormat("MMM d, y", Locale.getDefault())
+    formatter.timeZone = TimeZone.getDefault()
+    return formatter.format(dateValue)
+}
+
+fun parseDates(dateString: String): Date? {
+    /** Return a Date? object from an input Datetime String that is based in UTC
+     * */
+    //val dateStringParser = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'")
+    val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'", Locale.getDefault())
+    formatter.timeZone = TimeZone.getTimeZone("UTC")
+    return formatter.parse(dateString)
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun parseLocalDates(dateString: String): LocalDate {
+    return asLocalDate(parseDates(dateString)!!)
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun getNextContactLocalDate(contact: Contact): LocalDate {
+    /**
+     * Takes input of a Contact object and returns a LocalDate for the next expected reminder
+     * for that contact.
+     *
+     * Currently processes Contact date properties as DateStrings.
+     * TODO Refactor function after converting Contact date-property-types
+     */
+
+    // LocalDates of createdAt and lastContact, generated from date String
+    val lastContactLocalDate = contact.lastContacted?.let { parseLocalDates(it) }
+    val createdLocalDate = parseLocalDates(contact.createdAt)
+    /* Use these 2 instead of 2 above after converting Contact date-property-types
+    val lastContactLocalDate = contact.lastContacted?
+    val createdLocalDate = contact.createdAt
+      */
+
+    // Set starting point for next reminderDate based on lastContact or createdAt
+    var reminderDate = lastContactLocalDate ?: createdLocalDate
+    // Increment next reminderDate
+    when (contact.intervalUnit) {
+        "days" -> reminderDate = reminderDate.plusDays(contact.intervalTime.toLong())
+        "weeks" -> reminderDate = reminderDate.plusWeeks(contact.intervalTime.toLong())
+        "months" -> reminderDate = reminderDate.plusMonths(contact.intervalTime.toLong())
+        "years" -> reminderDate = reminderDate.plusYears(contact.intervalTime.toLong())
+    }
+    return reminderDate
+}
+//}
