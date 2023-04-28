22ffaeb06375b6030f039afff3a0d91de4e49f72
Matt Pereira <Mattcpereira@gmail.com>
2023-04-28 17:39:20 -0600

    Add Tests for errorPhone function into ContactListViewModelTest.kt

--- app/src/test/java/com/example/kit/ui/contactlist/ContactListViewModelTest.kt
+++ app/src/test/java/com/example/kit/ui/contactlist/ContactListViewModelTest.kt
@@ -12,6 +12,11 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ContactListViewModelTest {
+    /**
+     * Tests for ContactListViewModel functions.
+     * Test Naming convention is
+     * [functionName]_[descriptorOfTestCondition]_[expectedResult] ()
+     */
 
     private lateinit var viewModel: ContactListViewModel
 
@@ -27,7 +32,7 @@ class ContactListViewModelTest {
 
     // Following tests should raise "false" flags, indicating valid input
     @Test
-    fun errorEmail_nominal() {
+    fun errorEmail_nominal_false() {
         /**
          * errorEmail() test for valid, typical email address
          * (Including .com, .gov, .org, .edu, .me)
@@ -41,7 +46,7 @@ class ContactListViewModelTest {
         )
     }
     @Test
-    fun errorEmail_name_split() {
+    fun errorEmail_nameSplit_false() {
         /**
          * errorEmail() test for email address with . between names before @
          */
@@ -49,7 +54,7 @@ class ContactListViewModelTest {
     }
 
     @Test
-    fun errorEmail_country() {
+    fun errorEmail_country_false() {
         /**
          * errorEmail() test for email address with country extension
          * (e.g. @domain.com.br)
@@ -59,32 +64,74 @@ class ContactListViewModelTest {
 
     // Following tests should raise "true" flags, indicating invalid input
     @Test
-    fun errorEmail_no_at() {
+    fun errorEmail_noAt_true() {
         /**
          * errorEmail() test for email address without @ character
          */
         assertTrue(viewModel.errorEmail("MyNamedomain.com"))
     }
     @Test
-    fun errorEmail_no_domain() {
+    fun errorEmail_noDomain_true() {
         /**
          * errorEmail() test for email address without domain after @ character
          */
         assertTrue(viewModel.errorEmail("MyName@.com"))
     }
     @Test
-    fun errorEmail_no_domain_extension_split() {
+    fun errorEmail_noDomainExtensionSplit_true() {
         /**
          * errorEmail() test for email address without '.' between domain and extension
          */
         assertTrue(viewModel.errorEmail("MyName@domaincom"))
     }
     @Test
-    fun errorEmail_extension_dot() {
+    fun errorEmail_extensionDot_true() {
         /**
          * errorEmail() test for email address with ending '.' after extension
          */
         assertTrue(viewModel.errorEmail("MyName@domain.com."))
+    }
+
+    // Following tests should raise "false" flags, indicating valid input
+    @Test
+    fun errorPhoneNumber_nominal_false() {
+        /**
+         * errorPhoneNumber() test for typical 10-digit phone number
+         */
+        assertFalse(viewModel.errorPhoneNumber("3050285759"))
+        assertFalse(viewModel.errorPhoneNumber("0000000000"))
+    }
+
+    // Following tests should raise "true" flags, indicating invalid input
+    @Test
+    fun errorPhoneNumber_blank_true() {
+        /**
+         * errorPhoneNumber() test for a blank phone entry.
+         * There are currently checks (in Fragments) to
+         * prevent blanks from reaching this function.
+         */
+        assertTrue(viewModel.errorPhoneNumber(""))
+    }
+
+    @Test
+    fun errorPhoneNumber_wrongLength_true() {
+        /**
+         * errorPhoneNumber() test for phone with incorrect length
+         */
+        assertTrue(viewModel.errorPhoneNumber("0"))
+        assertTrue(viewModel.errorPhoneNumber("22"))
+        assertTrue(viewModel.errorPhoneNumber("333333333"))
+        assertTrue(viewModel.errorPhoneNumber("33333333344"))
+    }
+    @Test
+    fun errorPhoneNumber_countryCode_true() {
+        /**
+         * errorPhoneNumber() test for phone with country code included
+         */
+        assertTrue(viewModel.errorPhoneNumber("+133333334"))
+        assertTrue(viewModel.errorPhoneNumber("+10133333334"))
+        assertTrue(viewModel.errorPhoneNumber("+2013333333"))
+        assertTrue(viewModel.errorPhoneNumber("+20133333"))
     }
 
 }
